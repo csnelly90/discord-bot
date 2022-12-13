3c274c139c619c75af24610227d099375dc23f78
nelli <csnelly90@gmail.com>
2022-12-13 13:20:34 +0100

    add eventFiles and eventsPath to exported data in common.js

--- common.js
+++ common.js
@@ -1,11 +1,14 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-// helps to construct a path to the 'commands' directory
+// helps to construct a path to the selected directory
 const commandsPath = path.join(__dirname, 'commands');
+const eventsPath = path.join(__dirname, 'events');
 
 module.exports = {
     commandsPath: commandsPath,
+    eventsPath: eventsPath,
     // reads the path to the directory and returns an array of all the file names it contains
     commandFiles: fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')),
+    eventFiles: fs.readdirSync(eventsPath).filter(file => file.endsWith('.js')),
 };
